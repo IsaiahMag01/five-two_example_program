79e2993601547136ed3fd68f750c86906cbdaae4
Isaiah Magana <Isaiah2Mag@gmail.com>
2023-11-17 12:41:54 -0500

    Type out the javadoc comments for the program
    Type out all the neccesary java doc comments for the program

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Program which shows an example of a method from teh textbook chapter five section two
+ * @author(Isaiah Magana)
+ * Main.java
+ */
+
+public class Main {
+    /**
+     * Computes the volume of a cube
+     * @param sideLength the sidelength of the cube
+     * @return the volume of the cube
+     */
+    /**
+     * Main method to run program
+     */
+}
